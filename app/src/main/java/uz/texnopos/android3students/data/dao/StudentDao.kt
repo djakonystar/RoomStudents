dfa74c27be029344b811fbb12088454ef94e0e7a
Jaksibay Khakimov <djakonystar@MacBook-Pro-Jaksibay.local>
2022-06-16 09:59:25 +0500

    SearchView VS EditText

--- app/src/main/java/uz/texnopos/android3students/data/dao/StudentDao.kt
+++ app/src/main/java/uz/texnopos/android3students/data/dao/StudentDao.kt
@@ -8,4 +8,7 @@ import uz.texnopos.android3students.data.model.Student
 interface StudentDao {
     @Query("SELECT * FROM students")
     fun getAllStudents(): List<Student>
+
+    @Query("SELECT * FROM students WHERE name LIKE :searchValue")
+    fun searchStudents(searchValue: String): List<Student>
 }
