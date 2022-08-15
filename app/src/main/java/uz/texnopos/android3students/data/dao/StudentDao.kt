7eede1fa8698aaf43400232d15b1e4c8f0256f54
djakonystar <djakonystar911@gmail.com>
2022-08-15 15:22:39 +0500

    Migrated to RxJava3

--- app/src/main/java/uz/texnopos/android3students/data/dao/StudentDao.kt
+++ app/src/main/java/uz/texnopos/android3students/data/dao/StudentDao.kt
@@ -4,19 +4,21 @@ import androidx.room.Dao
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Observable
 import uz.texnopos.android3students.data.model.Student
 
 @Dao
 interface StudentDao {
     @Query("SELECT * FROM students")
-    fun getAllStudents(): List<Student>
+    fun getAllStudents(): Observable<List<Student>>
 
     @Query("SELECT * FROM students WHERE name LIKE :searchValue")
-    fun searchStudents(searchValue: String): List<Student>
+    fun searchStudents(searchValue: String): Observable<List<Student>>
 
     @Query("SELECT * FROM students WHERE is_favorite = 1")
-    fun getFavorites(): List<Student>
+    fun getFavorites(): Observable<List<Student>>
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
-    fun updateStudent(student: Student)
+    fun updateStudent(student: Student): Completable
 }
